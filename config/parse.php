dc1a2da2e44566bce08b2f19dc70c5a458c4816a
= <=>
2016-11-02 17:06:11 +0100

    create project

---
+++ config/parse.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of Laravel Parse.
+ *
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Parse App Id
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify your parse app id.
+    |
+    */
+
+    'app_id' => 'rUAYzYz5uSvrBthKcyAfQ5MQDyiiOg9SGVdM19iu',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Parse Rest Key
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify your parse rest key.
+    |
+    */
+
+    'rest_key' => 'QxWax2qkB4YLyNlYpS5yFaX23oKeIXNPQ6OFtPJG',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Parse Master Key
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify your parse master key.
+    |
+    */
+
+    'master_key' => 'ZLwL68YPPTWF2QHjNHhfeOavhPiLbUfNzgNalq0o',
+
+    'server_url' => 'https://api.parse.com/',
+
+];
